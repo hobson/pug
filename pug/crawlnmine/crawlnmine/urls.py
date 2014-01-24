10bbaad481c1236a2af4a6c19989f71b33be5bf5
Hobson Lane <hobsonlane@gmail.com>
2014-01-23 19:15:54 -0800

    moved views from crawler to miner

--- pug/crawlnmine/crawlnmine/urls.py
+++ pug/crawlnmine/crawlnmine/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from pug import crawler
+from pug.miner import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', crawler.views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     #url(r'^miner/', include('pug.miner.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- pug/miner/views.py
+++ pug/miner/views.py
@@ -1,3 +1,6 @@
+import json
+#from collections import Mapping
+
 from django.shortcuts import render_to_response
 
 from pug.nlp import parse
@@ -32,37 +35,37 @@ def connections(request, edges):
     return render_to_response('call_center/connections.html', data)
 
 
-def parse_node_name(name, use_defaults=False):
-    """
-    >>> sorted(parse_node_name('Origin,2.7, 3 ')[1].items())
-    [('charge', 2.7), ('group', 3), ('name', 'Origin')]
-    >>> parse_node_name('Origin,2.7, 3 ')[0]
-    'Origin'
-    """
-    # if the name is not a string, but a dict defining a node, then just set the defaults and return it
-    if isinstance(name, Mapping):
-        ans = dict(name)
-        for j, field in enumerate(parse_node_name.schema):
-            if field['key'] not in ans:
-                ans[field['key']] = field['default']
-        return ans
-    seq = listify(name, delim=',')
-    ans = {}
-    for j, field in enumerate(parse_node_name.schema):
-        if 'default' in field:
-            try:
-                ans[field['key']] = field['type'](seq[j])
-            except:
-                if use_defaults:
-                    ans[field['key']] = field['default']
-        else:
-            try:
-                ans[field['key']] = ans.get(field['key'], field['type'](seq[j]))
-            except:
-                pass
-    return ans
-parse_node_name.schema = (
-                {'key': 'name', 'type': str},  # TODO: use the absence of a default value (rather than index > 0) to identify mandatory fields
-                {'key': 'charge', 'type': float, 'default': 1},
-                {'key': 'group', 'type': intify, 'default': 0},  # TODO: this should be a string like the names/indexes to nodes (groups are just hidden nodes)
-              )
+# def parse_node_name(name, use_defaults=False):
+#     """
+#     >>> sorted(parse_node_name('Origin,2.7, 3 ')[1].items())
+#     [('charge', 2.7), ('group', 3), ('name', 'Origin')]
+#     >>> parse_node_name('Origin,2.7, 3 ')[0]
+#     'Origin'
+#     """
+#     # if the name is not a string, but a dict defining a node, then just set the defaults and return it
+#     if isinstance(name, Mapping):
+#         ans = dict(name)
+#         for j, field in enumerate(parse_node_name.schema):
+#             if field['key'] not in ans:
+#                 ans[field['key']] = field['default']
+#         return ans
+#     seq = listify(name, delim=',')
+#     ans = {}
+#     for j, field in enumerate(parse_node_name.schema):
+#         if 'default' in field:
+#             try:
+#                 ans[field['key']] = field['type'](seq[j])
+#             except:
+#                 if use_defaults:
+#                     ans[field['key']] = field['default']
+#         else:
+#             try:
+#                 ans[field['key']] = ans.get(field['key'], field['type'](seq[j]))
+#             except:
+#                 pass
+#     return ans
+# parse_node_name.schema = (
+#                 {'key': 'name', 'type': str},  # TODO: use the absence of a default value (rather than index > 0) to identify mandatory fields
+#                 {'key': 'charge', 'type': float, 'default': 1},
+#                 {'key': 'group', 'type': intify, 'default': 0},  # TODO: this should be a string like the names/indexes to nodes (groups are just hidden nodes)
+#               )
