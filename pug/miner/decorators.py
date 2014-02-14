e2144fdc4ef03a94fa51f8e3add0e613eb19210e
Hobson Lane <laneh@sharplabs.com>
2014-02-14 13:27:50 -0800

    arguments to db_name decorator

--- pug/miner/decorators.py
+++ pug/miner/decorators.py
@@ -4,7 +4,7 @@ import os
 import re
 
 
-def dbname_from_filename(cls, db_name='', alias_prefix=''):
+def dbname(cls, db_name='', alias_prefix=''):
     if not db_name:
         db_name = os.path.basename(getmodule(cls).__file__)
         db_name = re.sub(r'[.]pyc?$', '', flags=re.IGNORECASE)
@@ -12,7 +12,12 @@ def dbname_from_filename(cls, db_name='', alias_prefix=''):
         db_name = re.sub(r'^models[._-]+', '', flags=re.IGNORECASE)
     setattr(cls, '_db_alias', alias_prefix + db_name.lower())    
     setattr(cls, '_db_name', db_name)
-    return cls    
+    return cls
+
+
+def dbname_from_filename(cls, alias_prefix=''):
+    return dbname(cls, db_name=None, alias_prefix=alias_prefix)
+
 
 def represent(cls):
     setattr(cls, '__unicode__', representation)
