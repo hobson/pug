b38a33c5c05ce32d660e497f79d0ab1e647dc632
Hobson Lane <laneh@sharplabs.com>
2014-02-14 13:26:04 -0800

    arguments to db_name decorator

--- pug/miner/decorators.py
+++ pug/miner/decorators.py
@@ -1,16 +1,17 @@
 from pug.nlp.db import representation
 from inspect import getmodule
 import os
+import re
 
 
-def dbname_from_filename(cls):
-    fn = os.path.basename(getmodule(cls).__file__)
-    fn = fn[:-4] if fn.endswith('.pyc') else fn
-    fn = fn[:-3] if fn.endswith('.py') else fn
-    fn = fn[:-7] if fn.endswith('_models') else fn
-    fn = fn[7:] if fn.startswith('models_') else fn
-    setattr(cls, '_db_alias', 'SEC_' + fn.lower())    
-    setattr(cls, '_db_name', fn)
+def dbname_from_filename(cls, db_name='', alias_prefix=''):
+    if not db_name:
+        db_name = os.path.basename(getmodule(cls).__file__)
+        db_name = re.sub(r'[.]pyc?$', '', flags=re.IGNORECASE)
+        db_name = re.sub(r'[._-]+models$', '', flags=re.IGNORECASE)
+        db_name = re.sub(r'^models[._-]+', '', flags=re.IGNORECASE)
+    setattr(cls, '_db_alias', alias_prefix + db_name.lower())    
+    setattr(cls, '_db_name', db_name)
     return cls    
 
 def represent(cls):
