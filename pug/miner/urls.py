f83f31f9e3874e0728fbbbfee7124d3871d4a465
Hobson Lane <laneh@sharplabs.com>
2014-04-07 14:24:08 -0700

    moved template for linewithfocuschart

--- pug/miner/urls.py
+++ pug/miner/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 #from django.views.generic import TemplateView
 #import django.views.static
 #from views import JSONView
+from views import demo_linewithfocuschart, explorer
 
 
 from pug.miner.views import connections, home, StaticView
@@ -12,7 +13,7 @@ urlpatterns = patterns('',
     url(r'^(?:chart/)?(?:[Cc]onnect(?:ion)?s?|[Gg]raph)/(?P<edges>[^/]*)', connections),
     url(r'^$', 'pug.miner.views.home', name='home'),
     url(r'^explorer?/$', 'pug.miner.views.explorer', name='explorer'),
-    url(r'^linewithfocuschart/', 'demo_linewithfocuschart', name='demo_linewithfocuschart'),
+    url(r'^linewithfocuschart/', 'pug.miner.views.demo_linewithfocuschart', name='demo_linewithfocuschart'),
     #url(r'^static/(?P<path>.*)$', django.views.static.serve, { 'document_root': settings.STATIC_ROOT} ),
     #url(r'^media/(?P<path>.*)$', django.views.static.serve, { 'document_root': settings.MEDIA_ROOT} ),
     #url(r'^static/(?P<page>.*)\.json$', JSONView.as_view()),
