9b146ae367801a5f34c374217e96ab84ff2dd35c
Hobson Lane <laneh@sharplabs.com>
2014-09-16 10:41:36 -0700

    better docstring

--- pug/miner/templatetags/addcss.py
+++ pug/miner/templatetags/addcss.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# from http://vanderwijk.info/blog/adding-css-classes-formfields-in-django-templates/
+"""Template tag to add css class to the attributes of a field widget
+
+Example:
+
+{{field|addcssclass:"form-control"}}
+
+From:
+    http://vanderwijk.info/blog/adding-css-classes-formfields-in-django-templates/
+"""
 
 from django import template
 register = template.Library()
 
-@register.filter(name='addcss')
+@register.filter(name='addcssclass')
 def addcss(field, css):
    return field.as_widget(attrs={"class":css})
