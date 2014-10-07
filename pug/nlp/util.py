cbd2914341e4a043607ca1a4498333dcf01580b0
Hobson Lane <hobson@totalgood.com>
2014-10-06 22:54:27 -0700

    Docstring

--- pug/nlp/util.py
+++ pug/nlp/util.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''NLP utilities:
+'''Utilities to support natural language processing:
 
 * Vocabulary dimension reduction
 * Word statistics calculation
-* Adding a timezone to a datetime
+* Add a timezone to a datetime
+* Slice a django queryset
+* Genereate batches from a long list or sequence
+* Inverse dict/hashtable lookup
+* Generate a valid python variable or class name from a string
+* Generate a slidedeck-compatible markdown from an text or markdown outline or list
+* Convert a sequence of sequences to a dictionary of sequences
+* Pierson correlation coefficient calculation
+* Parse a string into sentences or tokens
+* Table (list of list) manipulation
 
 '''
 
