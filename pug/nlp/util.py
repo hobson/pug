6d89687d0ae0838d9fc73f88376a8e63ee187eca
Hobson Lane <hobson@totalgood.com>
2014-10-06 22:46:13 -0700

    Update util.py

--- pug/nlp/util.py
+++ pug/nlp/util.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''NLP utilities for use with django models and querysets and ORM (SQL)
+'''NLP utilities
+
+Dimension reduction
 
 Intended only for use within a Django project (requires django.db, which itself requires settings)
 
