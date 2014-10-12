77b6f0d260141a3363a6e9b2777ad5d255e3ac11
Hobson Lane <hobsonlane@gmail.com>
2014-10-12 11:52:18 -0700

    remove nltk dependency

--- pug/nlp/util.py
+++ pug/nlp/util.py
@@ -40,7 +40,7 @@ from pytz import timezone
 import numpy as np
 # import scipy as sci
 from fuzzywuzzy import process as fuzzy
-import nltk
+# import nltk
 from slugify import slugify
 # from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -2155,24 +2155,24 @@ def count_duplicates(items):
 
 
 
-def markdown_stats(doc):
-    """Compute statistics about the string or document provided.
+# def markdown_stats(doc):
+#     """Compute statistics about the string or document provided.
 
-    Returns:
-        dict: e.g. {'pages': 24, 'words': 1234, 'vocabulary': 123, 'reaading level': 3, ...}
-    """
-    sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    sentences = sentence_detector.tokenize(doc)
-    tokens = nltk.tokenize.punkt.PunktWordTokenizer().tokenize(doc)
-    vocabulary = collections.Counter(tokens)
+#     Returns:
+#         dict: e.g. {'pages': 24, 'words': 1234, 'vocabulary': 123, 'reaading level': 3, ...}
+#     """
+#     sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+#     sentences = sentence_detector.tokenize(doc)
+#     tokens = nltk.tokenize.punkt.PunktWordTokenizer().tokenize(doc)
+#     vocabulary = collections.Counter(tokens)
     
-    return collections.OrderedDict([
-        ('lines', sum([bool(l.strip().strip('-').strip()) for l in doc.split('\n')])),
-        ('pages', sum([bool(l.strip().startswith('---')) for l in doc.split('\n')]) + 1),
-        ('tokens', len(tokens)),
-        ('sentences', len(sentences)),
-        ('vocabulary', len(vocabulary.keys())),
-        ])
+#     return collections.OrderedDict([
+#         ('lines', sum([bool(l.strip().strip('-').strip()) for l in doc.split('\n')])),
+#         ('pages', sum([bool(l.strip().startswith('---')) for l in doc.split('\n')]) + 1),
+#         ('tokens', len(tokens)),
+#         ('sentences', len(sentences)),
+#         ('vocabulary', len(vocabulary.keys())),
+#         ])
 
 
 def slug_from_dict(d, max_len=128, delim='-'):
