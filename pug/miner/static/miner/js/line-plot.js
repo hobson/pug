3e36ce0e8223daa5097f90e0af37d8c7c7639cde
Hobson Lane <laneh@sharplabs.com>
2014-10-20 17:40:51 -0700

    line-plot-old.js is no longer old

--- pug/miner/static/miner/js/line-plot.js
+++ pug/miner/static/miner/js/line-plot.js
@@ -1,4 +1,3 @@
-
 function d3_parse_date(date_or_time) {
   return d3.time.format("%Y%m%d").parse(date_or_time);
 }
@@ -44,7 +43,6 @@ function query2obj(query) {
   return JSON.parse('{"' + decodeURI(query).replace(/"/g, '\\"').replace(/%2C/g,",").replace(/%2B/g," ").replace(/&/g, '","').replace(/=/g,'":"') + '"}')
   }
 
-
 function obj2query(obj, prefix) { 
     var str = [];
     for(var p in obj) {
@@ -56,7 +54,6 @@ function obj2query(obj, prefix) {
     return str.join("&");
 }
 
-
 function query_param(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -64,59 +61,93 @@ function query_param(name) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+// globals used by mouseover and mouseout for tooltips, etc
+var margin = {top: 30, right: 80, bottom: 30, left: 50};
+var  width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+var x_scale = d3.scale.linear().range([0, 1]);
+var y_scale = d3.scale.linear().range([1, 0]);
+var ylabel = "Vertical Value"
+var xlabel = "Horizontal Value (Time?)"
+
+// FIXME: put all globals in a plot conf object/namespace
+var conf = {margin: margin, width: width, height: height, x_scale: x_scale, xlabel: xlabel, y_scale: y_scale, ylabel: ylabel};
+
+console.log("conf");
+console.log(conf);
+// tooltips
+var svg = d3.select("#linegraph").append("svg")
+            .attr("width",  width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+console.log("svg");
+console.log(svg);
+var focus = svg.append("g")
+    .attr("transform", "translate(-100,-100)")  // make sure initial tool-tip circle is located outside (upper left) of the plot (svg element)
+    .attr("class", "focus");
+
+
+
+// query object used to set the HTTP GET query for the URI when the user clicks on a region/circle
+var query_obj = query2obj();
+delete query_obj.plot
+query_obj.table = "fast";
+
+
 function mouseover(d) {
   // displays tip at center of voronoi region instead of near point
   // tip.show(d);
 
+  console.log('mouseover')
+  console.log(d)
   // doesn't work
   d.series.line.parentNode.appendChild(d.series.line);
   d3.select(d.series.line).classed("series-hover", true);
 
-  // tip.attr("transform", "translate(" + x(d.x) + "," + y(d.y) + ")");
-  focus.attr("transform", "translate(" + x(d.x) + "," + y(d.y) + ")");
+  // tip.attr("transform", "translate(" + x_scale(d.x) + "," + y_scale(d.y) + ")");
+  console.log("transform", "translate(" + x_scale(d.x) + "," + y_scale(d.y) + ")");
+  focus.attr("transform", "translate(" + x_scale(d.x) + "," + y_scale(d.y) + ")");
   series_name = d.series.name.length ? d.series.name : ylabel
   tt = (xlabel.length ? xlabel : "bin") + ": " + d.x + "\u00A0\u00A0\u00A0\u00A0" + series_name + ": " + d.y;
   focus.select("text").text(tt);
 
-  query_obj.min_lag = d.x-5
-  query_obj.max_lag = d.x+5
+  query_obj.min_lag = d.x-5;
+  query_obj.max_lag = d.x+5;
 
+  // This generates the right link, but the SVG doesn't respond to clicks on the circle or anywhere nearby
   focus.select("a").attr("xlink:href", "?"+obj2query(query_obj));
-
+  console.log(focus.select("a"));
+  console.log(focus.select("a").attr("xlink:href"));
+  // FIXME: for this link to be visible/clickable the mouseout function has to be triggered when the mouse enters the circle and leaves the voronoi region
 }
 
 
-function mouseclick(d) {
-  console.log(d);
-  var url = document.URL + "&lag=" + d.x + "&series=" + d.series.name;
-  var hist_formats = ["", "-pmf", "-cmf", "-cfd"];
-  hist_formats.forEach(function(hf) { 
-    url = url.replace("/hist"+hf+"/", "/cases/");
-  });
-  var plot_types = ["linked", "link", "l", "zoomable", "zoom", "z"];
-  plot_types.forEach(function(pt) { 
-    url = url.replace("&plot="+pt, "&table=quick");
-  });
-  window.location = url;
-}
-
-
+// // FIXME: Unused!
+// function mouseclick(d) {
+//   console.log('mouseclick')
+//   console.log(d);
+//   var url = document.URL + "&lag=" + d.x + "&series=" + d.series.name;
+//   var hist_formats = ["", "-pmf", "-cmf", "-cfd"];
+//   hist_formats.forEach(function(hf) { 
+//     url = url.replace("/hist"+hf+"/", "/cases/");
+//   });
+//   var plot_types = ["linked", "link", "l", "zoomable", "zoom", "z"];
+//   plot_types.forEach(function(pt) { 
+//     url = url.replace("&plot="+pt, "&table=quick");
+//   });
+//   window.location = url;
+// }
 
 function mouseout(d) {
   // tip.hide(d);
+  console.log('mouseout')
   console.log(d);
   d3.select(d.series.line).classed("series-hover", false);
   focus.select("text").text("");
 }
 
-function d3_parse_date(date_or_time) {
-  return d3.time.format("%Y%m%d").parse(date_or_time);
-}
 
-
-var query_obj = query2obj();
-delete query_obj.plot
-query_obj.table = "fast";
 
 // Line plot with clickable Voronoi regions and mouse-over tool tips showing the coordinate values
 // 
@@ -128,199 +159,10 @@ query_obj.table = "fast";
 //     d3data[1][1..M] (Number): y-coordinate values
 //   x-axis (String, optional): horizontal x-axis label (overrides d3data[0][0])
 //   y-axis (String, optional): vertical y-axis label (overrides d3data[0][0])
-function line_plot_old(d3data, xlabel, ylabels) {
-
-    xlabel = typeof xlabel !== 'undefined' ? xlabel: "X Axis";
-    ylabels = typeof ylabels !== 'undefined' ? ylabels: ["Y Axis"];
-
-    // strip the header row, use the headers as axis labels, and transpose the data-series rows into columns
-    var data_obj = arrays_as_d3_series(d3data);
-    var xlabel = xlabel.length ? xlabel : data_obj.xlabel;
-    var ylabel = typeof ylabels[0] !== 'undefined' ? ylabels[0]: "Y Axis";
-    var ylabel = ylabel.length ? ylabel : data_obj.ylabels[0];
-    var ylabels = ylabels.length ? ylabels : data_obj.ylabels[0];
-
-    console.log('ylabels');
-    console.log(ylabels);
-    console.log('ylabel');
-    console.log(ylabel);
-    console.log('xlabel');
-    console.log(xlabel);
-
-    console.log('data_obj that contains the data bit')
-    console.log(data_obj)
-    var data = data_obj.data;
-    console.log('d3data after grabbing data bit')
-    console.log(data)
-
-    // be smarter about scaling margins with data and desired plot height/width
-    var margin = { top: 40, right: 80, bottom: 30, left: 50 };
-    var width = 960 - margin.left - margin.right;
-    var height = 500 - margin.top - margin.bottom;
-
-    // sort the data by the x-axis (typically time)
-    data.sort(function(a, b) { return a.x - b.x; });
-
-    var color = d3.scale.category10().domain(ylabels);
-
-    var x = d3.scale.linear().range([0, width]);
-
-    // parse xdata as datetimes if the xlabel starts with the word "date" or "time" 
-    if ((xlabel.substring(0, 4).toUpperCase() == "DATE") 
-        // || (xlabel.substring(0, 4).toUpperCase() == "TIME")
-      ) {
-      x = d3.time.scale().range([0, width]);
-      
-      data.forEach(function(d) { 
-        console.log(d);
-        console.log(d.x);
-        d.x = d3_parse_date(d.x); }
-        );
-    }
-
-    var xAxis = d3.svg.axis().scale(x).orient("bottom");
-
-    var y = d3.scale.linear().range([height, 0]);
-
-    var yAxis = d3.svg.axis().scale(y).orient("left");
-
-    var voronoi = d3.geom.voronoi()
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); })
-        .clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]);
-
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); });
-
-    // function make_smooth() {
-    //   line.interpolate("basis")
-    //   // FIXME: need to select and update the path using this new line object
-    // }
-
-    // function make_raw() {
-    //   line.interpolate(null)
-    //   // FIXME: need to select and update the path using this new line object
-    // }
-
-    // d3.select("#makesmooth").on("click", make_smooth);
-    // d3.select("#makeraw").on("click", make_smooth);
-
-
-    var svg = d3.select("#linegraph").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function line_plot(d3data, new_xlabel, new_ylabel) {
 
 
 
-
-    var all_series = color.domain().map(function(name) {
-      var series = { 
-        name: name,
-        values: null 
-      };
-      series.values = data.map(function(d) {
-            return {
-              series: series,
-              //name: name,  // unnecesary?
-              x: d.x,
-              y: +d[name]
-            }; // return {
-      }); // data.map(function(d) {
-      return series;
-    });
-
-
-    x.domain(d3.extent(data, function(d) { return d.x; }));
-
-    y.domain([
-      d3.min(all_series, function(c) { return d3.min(c.values, function(v) { return v.y; }); }),
-      d3.max(all_series, function(c) { return d3.max(c.values, function(v) { return v.y; }); })
-    ]);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .append("text")
-        .attr("y", y.range()[1])
-        .style("text-anchor", "end")
-        .attr("x", x.range()[1])
-        .attr("dy", "-.3em")
-
-        .text(xlabel);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".7em")
-        .style("text-anchor", "end")
-        .text(ylabel);
-
-    var series = svg.selectAll(".series")
-        .data(all_series)
-      .enter().append("g")
-        .attr("class", "series");
-
-    series.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) { d.line=this; return line(d.values); })
-        .style("stroke", function(d) { return color(d.name); });
-
-    // legend
-    series.append("text")
-        .datum(function(d) { return { name: d.name, value: d.values[d.values.length - 1]}; })
-        .attr("transform", function(d) { return "translate(" + x(d.value.x) + "," + y(d.value.y) + ")"; })
-        .attr("x", 3)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.name; });
-
-    var voronoiGroup = svg.append("g")
-        .attr("class", "voronoi");
-
-    voronoiGroup.selectAll("path")
-        .data(voronoi(d3.nest()
-            .key(function(d) { return x(d.x) + "," + y(d.y); })
-            .rollup(function(v) { return v[0]; })
-            .entries(d3.merge(all_series.map(function(d) { return d.values; })))
-            .map(function(d) { return d.values; })))
-      .enter().append("path")
-        .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-        .datum(function(d) { return d.point; })
-        .on("mouseover", mouseover)
-    //    .on("click", mouseclick)
-        .on("mouseout", mouseout);
-
-    // tooltips
-    var focus = svg.append("g")
-        .attr("transform", "translate(-100,-100)")  // make sure initial tool-tip circle is located outside (upper left) of the plot (svg element)
-        .attr("class", "focus");
-
-    focus.append("text").attr("y", -12);
-
-    focus.append("a").attr("xlink:href", "/")
-      .append("circle").attr("r", 4.5).style("fill", "steelblue").style("fill-opacity", 0.3);
-
-    var query_obj = query2obj();
-    delete query_obj.plot
-    query_obj.table = "fast";
-
-}
-
-
-
-
-function line_plot_old(d3data, xlabel, ylabel) {
-
-
-    var margin = {top: 20, right: 80, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
 
     // var d3data = {{ data.d3data|safe }};
 
@@ -328,9 +170,9 @@ function line_plot_old(d3data, xlabel, ylabel) {
     // var ylabel = "{{ data.ylabel|escapejs }}";
 
     var ans = arrays_as_d3_series(d3data);
-    var xlabel = xlabel.length ? xlabel : ans.xlabel;
+    xlabel = new_xlabel.length ? new_xlabel : ans.xlabel;
     var ylabels = [ylabel];
-    var ylabel = ylabel.length ? ylabel : ans.ylabels[0];
+    ylabel = new_ylabel.length ? new_ylabel : ans.ylabels[0];
     var data = ans.data;
     data.sort(function(a, b) { return a.x - b.x; });
 
@@ -342,14 +184,12 @@ function line_plot_old(d3data, xlabel, ylabel) {
 
     var color = d3.scale.category10().domain(ans.ylabels);
 
-
-   var x = d3.scale.linear().range([0, width]);
-
+    x_scale = d3.scale.linear().range([0, width]);
     // parse xdata as datetimes if the xlabel starts with the word "date" or "time" 
     if ((xlabel.substring(0, 4).toUpperCase() == "DATE") 
         // || (xlabel.substring(0, 4).toUpperCase() == "TIME")
       ) {
-      x = d3.time.scale().range([0, width]);
+      x_scale = d3.time.scale().range([0, width]);
       
       data.forEach(function(d) { 
         console.log(d);
@@ -358,27 +198,22 @@ function line_plot_old(d3data, xlabel, ylabel) {
         );
     }
 
-    var xAxis = d3.svg.axis().scale(x).orient("bottom");
+    y_scale = d3.scale.linear().range([height, 0]);
 
-    var y = d3.scale.linear().range([height, 0]);
+    var xAxis = d3.svg.axis().scale(x_scale).orient("bottom");
 
-    var yAxis = d3.svg.axis().scale(y).orient("left");
+    var yAxis = d3.svg.axis().scale(y_scale).orient("left");
 
     var voronoi = d3.geom.voronoi()
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); })
+        .x(function(d) { return x_scale(d.x); })
+        .y(function(d) { return y_scale(d.y); })
         .clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]);
 
     var line = d3.svg.line()
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); });
+        .x(function(d) { return x_scale(d.x); })
+        .y(function(d) { return y_scale(d.y); });
 
 
-    var svg = d3.select("#linegraph").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var all_series = color.domain().map(function(name) {
       var series = { 
@@ -397,9 +232,9 @@ function line_plot_old(d3data, xlabel, ylabel) {
     });
 
 
-    x.domain(d3.extent(data, function(d) { return d.x; }));
+    x_scale.domain(d3.extent(data, function(d) { return d.x; }));
 
-    y.domain([
+    y_scale.domain([
       d3.min(all_series, function(c) { return d3.min(c.values, function(v) { return v.y; }); }),
       d3.max(all_series, function(c) { return d3.max(c.values, function(v) { return v.y; }); })
     ]);
@@ -409,9 +244,9 @@ function line_plot_old(d3data, xlabel, ylabel) {
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
       .append("text")
-        .attr("y", y.range()[1])
+        .attr("y", y_scale.range()[1])
         .style("text-anchor", "end")
-        .attr("x", x.range()[1])
+        .attr("x", x_scale.range()[1])
         .attr("dy", "-.3em")
 
         .text(xlabel);
@@ -436,10 +271,11 @@ function line_plot_old(d3data, xlabel, ylabel) {
         .attr("d", function(d) { d.line=this; return line(d.values); })
         .style("stroke", function(d) { return color(d.name); });
 
+
     // legend
     series.append("text")
         .datum(function(d) { return { name: d.name, value: d.values[d.values.length - 1]}; })
-        .attr("transform", function(d) { return "translate(" + x(d.value.x) + "," + y(d.value.y) + ")"; })
+        .attr("transform", function(d) { return "translate(" + x_scale(d.value.x) + "," + y_scale(d.value.y) + ")"; })
         .attr("x", 3)
         .attr("dy", ".35em")
         .text(function(d) { return d.name; });
@@ -449,7 +285,7 @@ function line_plot_old(d3data, xlabel, ylabel) {
 
     voronoiGroup.selectAll("path")
         .data(voronoi(d3.nest()
-            .key(function(d) { return x(d.x) + "," + y(d.y); })
+            .key(function(d) { return x_scale(d.x) + "," + y_scale(d.y); })
             .rollup(function(v) { return v[0]; })
             .entries(d3.merge(all_series.map(function(d) { return d.values; })))
             .map(function(d) { return d.values; })))
@@ -460,21 +296,16 @@ function line_plot_old(d3data, xlabel, ylabel) {
     //    .on("click", mouseclick)
         .on("mouseout", mouseout);
 
-    // tooltips
-    var focus = svg.append("g")
+    // reappend the svg with a focus circle and see if it'll mouse-out from the veronoi
+    focus = svg.append("g")
         .attr("transform", "translate(-100,-100)")  // make sure initial tool-tip circle is located outside (upper left) of the plot (svg element)
         .attr("class", "focus");
 
     focus.append("text").attr("y", -12);
 
     focus.append("a").attr("xlink:href", "/")
-      .append("circle").attr("r", 4.5).style("fill", "steelblue").style("fill-opacity", 0.3);
-
+      .append("circle").attr("r", 6.5).style("fill", "steelblue").style("fill-opacity", 0.3);
 }
 
-
-var query_obj = query2obj();
-delete query_obj.plot
-query_obj.table = "fast";
 
 
