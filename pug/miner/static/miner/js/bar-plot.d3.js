c6436054318ab7e746af24dd28b9945af15c1b34
Hobson Lane <laneh@sharplabs.com>
2014-10-23 12:34:16 -0700

    modular style sheet for bar-plot.d3.js and bar_plot.d3.html

--- pug/miner/static/miner/js/bar-plot.d3.js
+++ pug/miner/static/miner/js/bar-plot.d3.js
@@ -152,24 +152,6 @@ function bar_plot(d3data, new_xlabel, new_ylabel) {
           .attr("width", x.rangeBand());
     }
 
-    // Inspired by Lee Byron's test data generator.
-    function bumpLayer(n, o) {
-
-      function bump(a) {
-        var x = 1 / (.1 + Math.random()),
-            y = 2 * Math.random() - .5,
-            z = 10 / (.1 + Math.random());
-        for (var i = 0; i < n; i++) {
-          var w = (i / n - y) * z;
-          a[i] += x * Math.exp(-w * w);
-        }
-      }
-
-      var a = [], i;
-      for (i = 0; i < n; ++i) a[i] = o + o * Math.random();
-      for (i = 0; i < 5; ++i) bump(a);
-      return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-    }
 } // function bar_plot(d3data)
 
 
