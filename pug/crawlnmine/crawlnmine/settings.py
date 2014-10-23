035cb51da28d2e9f6dbe0fe6f2020e3b0ccca120
Hobson Lane <laneh@sharplabs.com>
2014-10-23 09:49:37 -0700

    use run_gunicorn with foreman and Procfile

--- pug/crawlnmine/crawlnmine/settings.py
+++ pug/crawlnmine/crawlnmine/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'gunicorn',  # adds run_gunicorn command
     #'django_nvd3',
     # 'celery',
 
