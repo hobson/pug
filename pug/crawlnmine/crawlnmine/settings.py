1551c612fd4972f53f261ce90765e4a985ee228b
Hobson Lane <hobsonlane@gmail.com>
2014-11-23 21:56:09 -0800

    updated isntalled apps and requirements.txt for Django 1.7

--- pug/crawlnmine/crawlnmine/settings.py
+++ pug/crawlnmine/crawlnmine/settings.py
@@ -62,6 +62,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'gunicorn',  # adds run_gunicorn command
     #'django_nvd3',
     # 'celery',
