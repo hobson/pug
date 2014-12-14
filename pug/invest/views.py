1c808b5fd036ea5e3078072fa1040dd1043c10df
Hobson Lane <hobsonlane@gmail.com>
2014-12-13 21:09:23 -0800

    improve requirements, eliminate sim.py dependency in invest.views

--- pug/invest/views.py
+++ pug/invest/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 import datetime
 from django.views.generic import TemplateView
-import sim
+# import sim
 from pug.miner.views import d3_plot_context
-from models import Day, get_dataframes, get_panel
+from models import Day, get_panel #, get_dataframes, 
 from pug.nlp import util
 import pandas as pd
 
@@ -36,25 +36,25 @@ class PlotSymbolView(TemplateView):
             table=context['df'], title='Price History', xlabel='Date', ylabel='Adjusted Close')
 
 
-class PlotPredictionView(TemplateView):
-    """Query the miner.AggregateResults table to retrieve values for plotting in a bar chart"""
-    template_name = 'miner/line_plot.d3.html'
+# class PlotPredictionView(TemplateView):
+#     """Query the miner.AggregateResults table to retrieve values for plotting in a bar chart"""
+#     template_name = 'miner/line_plot.d3.html'
 
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(PlotSymbolView, self).get_context_data(**kwargs)
-        symbols = sim.normalize_symbols(self.kwargs['symbols'])
-        df = sim.price_dataframe(symbols=symbols,
-            start=datetime.datetime(2010, 1, 1),
-            end=datetime.datetime(2010, 12, 31),  
-            price_type='close')
-        # TODO: test!
-        qs = []
-        for i, symbol in enumerate(df.columns):
-            for row in df.records():
-                qs += [Day(date=datetime.date(row[0].year, row[0].month, row[0].day), close=row[i+1], symbol=symbol)]
-        Day.objects.bulk_create(qs)
+#     def get_context_data(self, **kwargs):
+#         # Call the base implementation first to get a context
+#         context = super(PlotSymbolView, self).get_context_data(**kwargs)
+#         symbols = sim.normalize_symbols(self.kwargs['symbols'])
+#         df = sim.price_dataframe(symbols=symbols,
+#             start=datetime.datetime(2010, 1, 1),
+#             end=datetime.datetime(2010, 12, 31),  
+#             price_type='close')
+#         # TODO: test!
+#         qs = []
+#         for i, symbol in enumerate(df.columns):
+#             for row in df.records():
+#                 qs += [Day(date=datetime.date(row[0].year, row[0].month, row[0].day), close=row[i+1], symbol=symbol)]
+#         Day.objects.bulk_create(qs)
+#         context['df'] = df
 
-        context['df'] = df
-        return d3_plot_context(context,
-            table=df, title='Price History', xlabel='Date', ylabel='Adjusted Close')
+#         return d3_plot_context(context,
+#             table=df, title='Price History', xlabel='Date', ylabel='Adjusted Close')
