4c0714edbeaf5613173b720fcab5587bec385ca7
Hobson Lane <hobsonlane@gmail.com>
2014-12-29 12:02:29 -0800

    finish TODO

--- pug/debug.py
+++ pug/debug.py
@@ -23,25 +23,38 @@
 #         code.interact(local=ns)
 
 import sys
+import traceback
 
-def bug_info(type, value, tb):
+def bug_info(exc_type, exc_value, exc_trace):
     """Prints the traceback and invokes the ipython debugger on any exception
+
+    Only invokes ipydb if you are outside ipython or python interactive session.
+    So scripts must be called from OS shell in order for exceptions to ipy-shell-out.
+
+    Dependencies:
+      Needs `pip install ipdb`
+
+    Arguments:
+      exc_type (type): The exception type/class (e.g. RuntimeError)
+      exc_value (Exception): The exception instance (e.g. the error message passed to the Exception constructor)
+      exc_trace (Traceback): The traceback instance
     
     References:
       http://stackoverflow.com/a/242531/623735
     """
     if hasattr(sys, 'ps1') or not sys.stderr.isatty():
         # We are in interactive mode or don't have a tty-like device, so we call the default hook
-        sys.__excepthook__(type, value, tb)
+        sys.__excepthook__(exc_type, exc_value, exc_trace)
     else:
-        # TODO: Why not import pdb earlier, outside this function ?
-        import traceback, ipdb
+        # Need to import non-built-ins here, so if dependencies haven't been installed, both tracebacks will print
+        # (e.g. the ImportError and the Exception that got you here)
+        import ipdb
         # We are NOT in interactive mode, print the exception
-        traceback.print_exception(type, value, tb)
+        traceback.print_exception(exc_type, exc_value, exc_trace)
         print
         # Start the debugger in post-mortem mode.
         # `ipdb.pm()` is deprecated so use `ipdb.post_mortem()` instead
-        ipdb.post_mortem(tb)
+        ipdb.post_mortem(exc_trace)
 
 # assign the `bug_info` function to the system exception hook/callback so it's called when there's an exception
 sys.excepthook = bug_info
