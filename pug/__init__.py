4d6f60a937ae9d238ef5895d16d5e34bbc74e38a
Hobson Lane <hobsonlane@gmail.com>
2015-03-07 08:33:01 -0800

    bump to 0.1.1

--- pug/__init__.py
+++ pug/__init__.py
@@ -19,7 +19,7 @@
 # THE SOFTWARE.
 """pug -- Package of Utilities written by and for the PDX Python User's Group"""
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __authors__ = [
     'Hobson <hobson@totalgood.com>'
     'Steve  <walkers@sharplabs.com>'
