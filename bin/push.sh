83d732733d664d1d786d396588b177660f0734c6
Hobson Lane <hobsonlane@gmail.com>
2015-03-13 11:18:02 -0700

    push all pug-* modules at once

--- bin/push.sh
+++ bin/push.sh
@@ -4,20 +4,25 @@
 
 cd ~/src/pug/
 git commit -am "$1"
+git pull
 git push
 
 cd ~/src/pug-nlp/
 git commit -am "$1"
+git pull
 git push
 
 cd ~/src/pug-dj/
 git commit -am "$1"
+git pull
 git push
 
 cd ~/src/pug-ann/
 git commit -am "$1"
+git pull
 git push
 
 cd ~/src/pug-invest/
 git commit -am "$1"
+git pull
 git push
