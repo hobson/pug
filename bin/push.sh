cb02fd24798dcac4316c041615689371a6efd1ba
Hobson Lane <hobsonlane@gmail.com>
2015-03-13 11:16:09 -0700

    bash script to push all branches

---
+++ bin/push.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# use bash array to do this right or do it in python
+
+cd ~/src/pug/
+git commit -am "$1"
+git push
+
+cd ~/src/pug-nlp/
+git commit -am "$1"
+git push
+
+cd ~/src/pug-dj/
+git commit -am "$1"
+git push
+
+cd ~/src/pug-ann/
+git commit -am "$1"
+git push
+
+cd ~/src/pug-invest/
+git commit -am "$1"
+git push
